1c62cba7b07abcb7ff8ca72d915060e80fdebe3e
Bao Lam <90415233+TanTamBao-Lam@users.noreply.github.com>
2023-11-13 23:06:08 -0400

    Add rent model

--- BikeRental/BikeRentalApp/models.py
+++ BikeRental/BikeRentalApp/models.py
@@ -42,5 +42,36 @@ class Renter(models.Model):
     # Renter string rep.
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} - #{self.phone}"
+    
+# Rental model
+class Rental(models.Model):
+    # Bike field is a foreign key to Bike model.
+    bike = models.ForeignKey(Bike, on_delete=models.CASCADE)
+    
+    # Renter field is a foreign key to Renter model.
+    renter = models.ForeignKey(Renter, on_delete=models.CASCADE)
+    
+    # Rent date field.
+    date = models.DateField(default=datetime.date.today)
+    
+    # Price field
+    price = models.DecimalField(default=0.0)
+    
+    # Method for calculating price.
+    def calc_price(self) -> float:
+        curr_price = BASE_PRICE
+        
+        # if bike type is Tandem or Electric, there will be surcharge based on vehicle type.
+        if self.bike.bike_type == "TA":
+            curr_price += TANDEM_SURCHARGE
+        if self.bike.bike_type == "EL":
+            curr_price += ELECTRIC_SURCHARGE    
+        # if renter is vip, discount current price 20%.
+        if self.renter.vip_num > 0:
+            curr_price *= .8
+            
+        # set price.
+        self.price = curr_price
+        
 
 # endregion
